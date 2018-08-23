5e7269e6d344d0bf2280ca39642ecd2ccf166390
otake-github <m.otake.develop@gmail.com>
2018-08-24 03:31:51 +0900

    serve

--- server/app.ts
+++ server/app.ts
@@ -30,7 +30,8 @@ export const AppFactory = {
 
         // Express configuration
         app.set('port', process.env.PORT || 3000);
-        app.set('views', path.join(__dirname, '../views'));
+        // app.set('views', path.join(__dirname, '../views'));
+        app.set('views', path.join(__dirname, '../../views'));
         app.set('view engine', 'pug');
         app.use(compression());
         app.use(bodyParser.json());
